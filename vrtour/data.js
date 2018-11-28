dcfbad38e744fb367ad63f234c23775e62a9269e
ejilay <ivbaturin@gmail.com>
2018-11-28 19:30:28 +0300

    :star: new: vr tour

---
+++ vrtour/data.js
@@ -0,0 +1,273 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "ЖК Сколковский Офис продаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -2.824653472922808,
+        "pitch": -0.03622714179975972,
+        "fov": 0.6073509984652316
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.4684561501650037,
+          "pitch": -0.09915564371966568,
+          "title": "Макет",
+          "text": "Дорогущий макет"
+        },
+        {
+          "yaw": 1.9950242730356456,
+          "pitch": 0.08567338755623766,
+          "title": "Присядьте, пожалуйста",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7694076400800434,
+          "pitch": -0.13685605331677309,
+          "title": "Оформление договоров",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.9007454112506714,
+          "pitch": -0.031690123117499525,
+          "title": "Title",
+          "text": "Водичка"
+        }
+      ]
+    },
+    {
+      "id": "1-1--",
+      "name": "1-шка Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1--",
+      "name": "1-шка Кухня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1--",
+      "name": "1-шка СУ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-1--1",
+      "name": "1-шка Комната1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-1--2",
+      "name": "1-шка Комната2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0001866186998906727,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": -0.9821260473893236,
+          "pitch": -0.2437688783000862,
+          "rotation": 0,
+          "target": "0----"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.000362432300040183,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-1--3",
+      "name": "1-шка Комната3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ЖК Сколковский",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
